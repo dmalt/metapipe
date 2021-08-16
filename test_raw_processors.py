100208c93515f277f73cb2f0629c37a635c2e5a0
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-08-16 14:27:15 +0300

    Add tests for chain and cat

--- test_raw_processors.py
+++ test_raw_processors.py
@@ -11,6 +11,10 @@ from raw_processors import (
     Resample,
     FifReader,
     MneWriter,
+    RawReader,
+    RawWriter,
+    RawProcessor,
+    RawProcessorsChain
 )
 
 
@@ -28,12 +32,17 @@ def simple_raw_factory():
 
 
 @fixture
-def saved_fif_fpath_and_object(simple_raw_factory, tmp_path):
+def saved_fif_fpath_and_object(simple_raw_factory, tmp_raw_savepath):
     raw = simple_raw_factory(4, 300, 32)
-    savepath = tmp_path / "raw.fif"
-    raw.save(savepath)
-    yield savepath, raw
-    savepath.unlink()
+    raw.save(tmp_raw_savepath)
+    return tmp_raw_savepath, raw
+
+
+@fixture
+def tmp_raw_savepath(tmp_path):
+    dest_path = tmp_path / "raw.fif"
+    yield dest_path
+    dest_path.unlink()
 
 
 def test_band_pass_filter_filters_data(simple_raw_factory):
@@ -53,6 +62,13 @@ def test_concat_with_same_channels(simple_raw_factory):
     res = cat.run(raw, raw)
     assert res.n_times == raw.n_times * 2
     assert len(res.ch_names) == len(raw.ch_names)
+
+
+def test_concat_single_object(simple_raw_factory):
+    raw = simple_raw_factory(1, 300, 33)
+    cat = ConcatRaws()
+    res = cat.run(raw)
+    assert_allclose(res.get_data(), raw.get_data())
 
 
 def test_cat_with_different_channels_intersects_channels(simple_raw_factory):
@@ -85,10 +101,64 @@ def test_fif_reader_reads_same_data(saved_fif_fpath_and_object):
     assert_allclose(loaded_raw.get_data(), saved_raw.get_data())
 
 
-def test_mne_writer_data_unchanged(simple_raw_factory, tmp_path):
+def test_mne_writer_data_unchanged(simple_raw_factory, tmp_raw_savepath):
     raw = simple_raw_factory(1, 300, 32)
     writer = MneWriter()
-    savepath = tmp_path / "raw.fif"
-    writer.write(raw, savepath)
-    loaded_raw = read_raw_fif(savepath)
+    writer.write(raw, tmp_raw_savepath)
+    loaded_raw = read_raw_fif(tmp_raw_savepath)
     assert_allclose(raw.get_data(), loaded_raw.get_data())
+
+
+@fixture
+def mock_reader():
+    class MockRawReader(RawReader):
+        def read(self, path):
+            return read_raw_fif(path, preload=True)
+
+    return MockRawReader(config={})
+
+
+@fixture
+def mock_writer():
+    class MockWriter(RawWriter):
+        def write(self, raw, path):
+            raw.save(path)
+
+    return MockWriter(config={})
+
+
+@fixture
+def mock_processor():
+    class MockProcessor(RawProcessor):
+        """Append somethng to raw.info['description']"""
+
+        def run(self, raw):
+            cp = raw.copy()
+            if cp.info["description"]:
+                cp.info["description"] += ", mock-processed"
+            else:
+                cp.info["description"] = "mock-processed"
+
+            return cp
+
+    return MockProcessor()
+
+
+def test_raw_processors_chain(
+    mock_reader,
+    mock_writer,
+    mock_processor,
+    saved_fif_fpath_and_object,
+):
+    raw_path = saved_fif_fpath_and_object[0]
+    savepath = raw_path.parent / ("mock_processed_" + raw_path.name)
+    chain = RawProcessorsChain(
+        [raw_path],
+        savepath,
+        mock_reader,
+        [mock_processor, mock_processor],
+        mock_writer,
+    )
+    chain.run()
+    raw_loaded = read_raw_fif(savepath)
+    assert raw_loaded.info['description'] == "mock-processed, mock-processed"
