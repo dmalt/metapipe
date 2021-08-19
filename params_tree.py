3ac025f81be2d5ab1180196f66441a081f820ba9
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-08-19 17:28:57 +0300

    Refactor + add __str__ method

--- params_tree.py
+++ params_tree.py
@@ -1,6 +1,11 @@
 from collections import OrderedDict
 from dataclasses import dataclass, field
 from typing import Any, Iterable, List
+from itertools import chain
+
+
+INDENT = " " * 4
+SEP = ": "
 
 
 class LevelError(Exception):
@@ -8,40 +13,51 @@ class LevelError(Exception):
 
 
 @dataclass
-class ParamsTreeNode:
+class _ParamsTreeNode:
     level: str
     value: Any
-    _children: list = field(default_factory=list, init=False, repr=False)
+    _children: List = field(default_factory=list, init=False, repr=False)
+
+    def __str__(self):
+        str_builder = [f"{self.level}{SEP}{self.value}"]
+        if self.children:
+            str_builder.append(
+                "\n".join(
+                    "\n".join(map(lambda k: INDENT + k, str(c).split("\n")))
+                    for c in self.children
+                )
+            )
+        return "\n".join(str_builder)
 
     @property
     def children(self):
         return self._children
 
-    @classmethod
-    def _create_level(cls, level_name, values):
-        return [cls(level_name, v) for v in values]
-
-    def flatten_keys(self) -> Iterable[OrderedDict]:
+    def flatten(self) -> Iterable[OrderedDict]:
         if self._is_leaf_node():
             yield OrderedDict({self.level: self.value})
         for c in self.children:
-            for k in c.flatten_keys():
+            for k in c.flatten():
                 yield {self.level: self.value} | k
 
-    def _change_outermost_level(self, values, filt: dict):
+    def change_last_level(self, values, level_filters: dict = None):
+        level_filters = {} if level_filters is None else level_filters
         if self._is_next_to_leaf_node():
-            level_name = self._children[0].level
-            self._children = self._create_level(level_name, values)
+            self._children = self._create_level(self.children[0].level, values)
         else:
-            for c in filter(lambda c: c.level not in filt or filt[c.level](c.value), self.children):
-                c._change_outermost_level(values, filt)
+            for c in self._filt_children(level_filters):
+                c.change_last_level(values, level_filters)
 
-    def _append_level(self, level_name, values):
+    def append_level(self, level_name, values):
         if self._is_leaf_node():
             self._children = self._create_level(level_name, values)
         else:
             for c in self.children:
-                c._append_level(level_name, values)
+                c.append_level(level_name, values)
+
+    @classmethod
+    def _create_level(cls, level_name, values):
+        return [cls(level_name, v) for v in values]
 
     def _is_leaf_node(self):
         return not self._children
@@ -49,29 +65,43 @@ class ParamsTreeNode:
     def _is_next_to_leaf_node(self):
         return self._children and not self.children[0]._children
 
+    def _filt_children(self, filt):
+        return filter(
+            lambda c: c.level not in filt or filt[c.level](c.value),
+            self.children,
+        )
+
 
 @dataclass
-class ParamsTree(ParamsTreeNode):
-    _level_names: List[str] = field(init=False, repr=False)
+class ParamsTree:
+    _levels: List[str] = field(init=False, repr=False, default_factory=list)
+    _root: _ParamsTreeNode = field(init=False, repr=False)
 
     @property
     def level_names(self):
-        return self._level_names
+        return self._levels
 
     def __post_init__(self):
-        self._level_names = [self.level]
+        self._root = _ParamsTreeNode("ROOT", None)
 
-    def append_level(self, level_name, values):
+    def __str__(self):
+        return "\n".join(str(c) for c in self._root.children)
+
+    def flatten(self):
+        return chain(*[c.flatten() for c in self._root.children])
+
+    def append(self, level_name, values):
         if level_name in self.level_names:
             raise LevelError(f"Level '{level_name}' exists")
-        self._append_level(level_name, values)
-        self._level_names.append(level_name)
+        self._root.append_level(level_name, values)
+        self._levels.append(level_name)
 
-    def change_outermost_level(self, values, filt):
-        self._check_filter_levels(filt)
-        self._change_outermost_level(values, filt)
+    def change_last(self, values, level_filters=None):
+        self._check_filter_levels_present(level_filters)
+        self._root.change_last_level(values, level_filters)
 
-    def _check_filter_levels(self, filt):
-        for level in filt:
-            if level not in self._level_names:
-                raise LevelError(f"Level {level} not found")
+    def _check_filter_levels_present(self, level_filters):
+        if level_filters is None:
+            return
+        if any(level for level in level_filters if level not in self._levels):
+            raise LevelError("One of filter levels not found")
--- test_params_tree.py
+++ test_params_tree.py
@@ -2,83 +2,66 @@ from collections import OrderedDict
 
 from pytest import fixture, raises
 
-from params_tree import ParamsTree, LevelError
+from params_tree import ParamsTree, LevelError, INDENT, SEP
 
 
 @fixture
-def tree_factory():
-    def wrapped(level, value):
-        return ParamsTree(level=level, value=None)
-
-    return wrapped
+def tree():
+    return ParamsTree()
 
 
-def test_constuctor_sets_attributes(tree_factory):
-    tree = tree_factory(level="root", value=None)
-    assert tree.level == "root"
-    assert tree.value is None
-    assert tree.level_names == ["root"]
-
-
-def test_add_single_layer_sets_children_and_appends_level_name(tree_factory):
+def test_add_single_layer_sets_children_and_appends_level_name(tree):
     subjects = ("01", "02", "emptyroom")
     level = "sub"
-    tree = tree_factory(level="root", value=None)
-    tree.append_level(level, subjects)
+    tree.append(level, subjects)
     assert tree.level_names[-1] == level
-    for child in tree.children:
-        assert child.level == level
-        assert child.value in subjects
+    flat_keys = list(tree.flatten())
+    assert flat_keys == [OrderedDict({"sub": s}) for s in subjects]
 
 
-def test_appending_existing_layer_raises_exception(tree_factory):
-    tree = tree_factory("root", None)
+def test_appending_existing_layer_raises_exception(tree):
+    tree.append("root", (None,))
     with raises(LevelError):
-        tree.append_level("root", (None,))
+        tree.append("root", (None,))
 
 
-def test_flatten_keys(tree_factory):
-    tree = tree_factory(level="root", value=None)
-    subjects = ("01", "02", "emptyroom")
-    level = "sub"
-    tree.append_level(level, subjects)
-    flat_keys = list(tree.flatten_keys())
-    assert flat_keys == [
-        OrderedDict({"root": None, "sub": "01"}),
-        OrderedDict({"root": None, "sub": "02"}),
-        OrderedDict({"root": None, "sub": "emptyroom"}),
-    ]
+def test_change_outermost_level_no_filter(tree):
+    tree.append("sub", ("01",))
+    tree.append("task", ("eo", "ec"))
+    tree.change_last((None,))
+
+    keys = list(tree.flatten())
+    assert keys == [OrderedDict({"sub": "01", "task": None})]
 
 
-def test_change_outermost_level_no_filter(tree_factory):
-    tree = tree_factory("root", None)
-    tree.append_level("sub", ("01",))
-    tree.append_level("task", ("eo", "ec"))
-    tree.change_outermost_level((None,), filt={})
-
-    keys = list(tree.flatten_keys())
-    assert keys == [OrderedDict({"root": None, "sub": "01", "task": None})]
-
-
-def test_change_outermost_level_filter(tree_factory):
-    tree = tree_factory("root", None)
-    tree.append_level("sub", ("01", "emptyroom"))
-    tree.append_level("task", ("eo",))
-    tree.change_outermost_level(
-        ("noise",), filt={"sub": lambda s: s == "emptyroom"}
+def test_change_outermost_level_filter(tree):
+    tree.append("sub", ("01", "emptyroom"))
+    tree.append("task", ("eo",))
+    tree.change_last(
+        ("noise",), level_filters={"sub": lambda s: s == "emptyroom"}
     )
-    keys = list(tree.flatten_keys())
+    keys = list(tree.flatten())
     assert keys == [
-        OrderedDict({"root": None, "sub": "01", "task": "eo"}),
-        OrderedDict({"root": None, "sub": "emptyroom", "task": "noise"}),
+        OrderedDict({"sub": "01", "task": "eo"}),
+        OrderedDict({"sub": "emptyroom", "task": "noise"}),
     ]
 
 
-def test_change_outermost_level_with_nonexistent_filter_raises_exception(tree_factory):
-    tree = tree_factory("root", None)
-    tree.append_level("sub", ("01", "emptyroom"))
-    tree.append_level("task", ("eo",))
+def test_change_outermost_level_with_nonexistent_filter_raises_exception(tree):
+    tree.append("sub", ("01", "emptyroom"))
+    tree.append("task", ("eo",))
     with raises(LevelError):
-        tree.change_outermost_level(
-            values=["noise"], filt={"run": lambda s: s == "01"}
-        )
+        tree.change_last(["noise"], {"run": lambda s: s == "01"})
+
+
+def test_print(tree):
+    tree.append("sub", ("01", "emptyroom"))
+    tree.append("task", ("eo", "ec"))
+    assert str(tree) == (
+        f"sub{SEP}01\n"
+        + f"{INDENT}task{SEP}eo\n"
+        + f"{INDENT}task{SEP}ec\n"
+        + f"sub{SEP}emptyroom\n"
+        + f"{INDENT}task{SEP}eo\n"
+        + f"{INDENT}task{SEP}ec"
+    )
