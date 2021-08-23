438b2aa556627ca4fb2ef0376f78c3d6312eb7c4
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-08-23 19:31:52 +0300

    Start from scratch

--- metapipe/params_tree.py
+++ metapipe/params_tree.py
@@ -1,151 +1,24 @@
 from abc import ABC, abstractmethod
 from collections import OrderedDict
 from dataclasses import dataclass, field
-from itertools import chain
-from typing import (
-    Any,
-    Collection,
-    Generator,
-    Hashable,
-    Iterator,
-    List,
-    Dict,
-    Callable,
-)
+from typing import Sequence, Union
+
+Value = Union[str, int, None]
 
 
-class ParamsTreeError(Exception):
-    pass
-
-
-class LevelError(ParamsTreeError):
+class ParamsTrieError(Exception):
     pass
 
 
 @dataclass
-class ViewableNode:
-    level: str
-    value: Hashable
-    children: List = field(default_factory=list, init=False, repr=False)
+class ParamsTrie:
+    @dataclass
+    class _ParamsNode:
+        key: str
+        value: Value
+        children: Sequence
 
-    def is_leaf_node(self):
-        return not self.children
-
-    def is_next_to_leaf_node(self):
-        return self.children and not self.children[0].children
-
-
-class ViewableTree(ABC):
-    root: ViewableNode = field(init=False, repr=False)
-
-
-class View(ABC):
-    @abstractmethod
-    def get(self, tree: ViewableTree) -> Any:
-        """Get the view"""
-
-
-class FlatView(View):
-    def get(self, tree: ViewableTree) -> List[OrderedDict]:
-        return list(chain(*(self.flatten(c) for c in tree.root.children)))
-
-    @staticmethod
-    def flatten(node: ViewableNode) -> Generator[OrderedDict, None, None]:
-        if node.is_leaf_node():
-            yield OrderedDict({node.level: node.value})
-        for c in node.children:
-            for k in FlatView.flatten(c):
-                yield OrderedDict({node.level: node.value} | k)
-
-
-@dataclass
-class StringView(View):
-    TAB: str = " " * 4
-    SEP: str = ": "
-
-    def get(self, tree: ViewableTree) -> str:
-        return "\n".join(self._to_str(c) for c in tree.root.children)
-
-    def _to_str(self, node: ViewableNode) -> str:
-        res = [f"{node.level}{self.SEP}{node.value}"]
-        res.extend(self._indent(self._to_str(c)) for c in node.children)
-        return "\n".join(res)
-
-    def _indent(self, node_str: str) -> str:
-        return "\n".join(map(lambda k: self.TAB + k, node_str.split("\n")))
-
-
-class ParamsNode(ViewableNode):
-    def __getitem__(self, item: Any) -> ViewableNode:
-        for c in self.children:
-            if c.value == item:
-                return c
-        raise ParamsTreeError(f"Item {item} not found")
-
-    @classmethod
-    def create_level(cls, level_name: str, values: Collection[Any]) -> List:
-        return [cls(level_name, v) for v in values]
-
-
-@dataclass
-class ParamsTree(ViewableTree):
-    root: ParamsNode = field(init=False, repr=False)
-    str_view: StringView = field(default_factory=StringView)
-    levels: List[str] = field(init=False, repr=False, default_factory=list)
+    root: _ParamsNode = field(init=False, repr=False)
 
     def __post_init__(self):
-        self.root = ParamsNode("ROOT", None)
-
-    def append(self, level_name: str, values: Collection[Hashable]) -> None:
-        """Append level to a tree"""
-        if level_name in self.levels:
-            raise LevelError(f"Level '{level_name}' exists")
-        self._check_values_unique(values)
-        self.levels.append(level_name)
-        self._append(self.root, level_name, values)
-
-    def change_last(
-        self,
-        values: Collection[Hashable],
-        level_filters: Dict[str, Callable] = None,
-    ) -> None:
-        """Selectively change last level values"""
-        level_filters = {} if level_filters is None else level_filters
-        self._check_filter_levels_present(level_filters)
-        self._check_values_unique(values)
-        self._change_last(self.root, values, level_filters)
-
-    def __str__(self) -> str:
-        return self.str_view.get(self)
-
-    def _append(self, node, level_name, values):
-        if node.is_leaf_node():
-            node.children = self.root.create_level(level_name, values)
-        else:
-            for child in node.children:
-                self._append(child, level_name, values)
-
-    def _change_last(self, node, values, level_filters):
-        if node.is_next_to_leaf_node():
-            node.children = node.create_level(node.children[0].level, values)
-        else:
-            for c in self._filt_children(node, level_filters):
-                self._change_last(c, values, level_filters)
-
-    def _check_filter_levels_present(self, level_filters):
-        if any(level for level in level_filters if level not in self.levels):
-            raise LevelError(f"Each filter level must be one of {self.levels}")
-
-    @staticmethod
-    def _filt_children(node, filt) -> Iterator:
-        return filter(
-            lambda c: c.level not in filt or filt[c.level](c.value),
-            node.children,
-        )
-
-    @staticmethod
-    def _check_values_unique(values) -> None:
-        if len(set(values)) != len(values):
-            raise ParamsTreeError(
-                f"All values must be different (got '{values}')"
-            )
+        self.root = self._ParamsNode("ROOT", None)
--- metapipe/tests/test_params_tree.py
+++ metapipe/tests/test_params_tree.py
@@ -2,89 +2,9 @@ from collections import OrderedDict
 
 from pytest import fixture, raises
 
-from metapipe.params_tree import (
-    ParamsTree,
-    LevelError,
-    FlatView,
-    ParamsNode,
-    ParamsTreeError,
-)
+from metapipe.params_tree import ParamsTrie, ParamsTrieError
 
 
 @fixture
 def tree():
-    return ParamsTree()
-
-
-def test_params_node_getitem_raises_exception_when_no_item(tree):
-    with raises(ParamsTreeError):
-        tree.root["something"]
-
-
-def test_add_single_layer_sets_children_and_appends_level_name(tree):
-    subjects = ("01", "02", "emptyroom")
-    level = "sub"
-    tree.append(level, subjects)
-    assert tree.levels[-1] == level
-    for s in subjects:
-        assert tree.root[s] == ParamsNode(level, s)
-
-
-def test_appending_existing_layer_raises_exception(tree):
-    tree.append("root", (None,))
-    with raises(LevelError):
-        tree.append("root", (None,))
-
-
-def test_appending_layer_with_nonunique_values_raises_exception(tree):
-    with raises(ParamsTreeError):
-        tree.append("sub", ("01", "01"))
-
-
-def test_changing_last_with_nonunique_values_raises_exception(tree):
-    tree.append("sub", ("01", "02"))
-    with raises(ParamsTreeError):
-        tree.change_last(("01", "01"))
-
-
-def test_change_outermost_level_no_filter(tree):
-    tree.append("sub", ("01",))
-    tree.append("task", ("eo", "ec"))
-    tree.change_last((None,))
-
-    assert tree.root["01"][None] == ParamsNode("task", None)
-
-
-def test_change_outermost_level_filter(tree):
-    tree.append("sub", ("01", "emptyroom"))
-    tree.append("task", ("eo",))
-    tree.change_last(
-        ("noise",), level_filters={"sub": lambda s: s == "emptyroom"}
-    )
-    keys = FlatView().get(tree)
-    assert keys == [
-        OrderedDict({"sub": "01", "task": "eo"}),
-        OrderedDict({"sub": "emptyroom", "task": "noise"}),
-    ]
-
-
-def test_change_outermost_level_with_nonexistent_filter_raises_exception(tree):
-    tree.append("sub", ("01", "emptyroom"))
-    tree.append("task", ("eo",))
-    with raises(LevelError):
-        tree.change_last(["noise"], {"run": lambda s: s == "01"})
-
-
-def test_print(tree):
-    tree.append("sub", ("01", "emptyroom"))
-    tree.append("task", ("eo", "ec"))
-    INDENT = " " * 4
-    SEP = ": "
-    assert str(tree) == (
-        f"sub{SEP}01\n"
-        + f"{INDENT}task{SEP}eo\n"
-        + f"{INDENT}task{SEP}ec\n"
-        + f"sub{SEP}emptyroom\n"
-        + f"{INDENT}task{SEP}eo\n"
-        + f"{INDENT}task{SEP}ec"
-    )
+    return ParamsTrie()
